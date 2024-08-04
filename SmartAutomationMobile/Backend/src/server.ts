ad839c18a33e21fd5ad30568c85fd50cb3b3abc7
Dursky <ctzg12@gmail.com>
2024-08-04 22:42:29 +0200

    fix: clearing repo at mobile

--- SmartAutomationMobile/Backend/src/server.ts
+++
@@ -1,8 +0,0 @@
-import { app, httpServer } from './app';
-import config from './config';
-
-const PORT = config.port;
-
-httpServer.listen(PORT, () => {
-  console.log(`-> Server is running on port ${PORT}`);
-});
